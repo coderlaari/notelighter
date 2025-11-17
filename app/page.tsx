5a611623b1f1f7c4c9e75f521fa06788612deb7f
Lauri Letonsaari <lauri.letonsaari@gmail.com>
2025-11-17 14:20:51 +0200

    Home, Notes and bunch of more things added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { SignedIn, useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { user } = useUser();
+  if (user) {
+    redirect("/home");
+  }
   return (
-    <div>
-      <Button>Hello</Button>
-    </div>
+    <main className="text-center w-full h-full p-5">
+      <p className="text-3xl">Welcome to NoteLighter!</p>
+      <p className="text-2xl">NoteLighter isn't just any notetaking app.</p>
+    </main>
   );
 }
---
+++ components/app-navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+export function Navbar() {
+  return (
+    <nav className="bg-gray-800 h-12 w-full flex gap-3 p-1.5">
+      <SignedIn>
+        <Button>
+          <Link href="/home">Home</Link>
+        </Button>
+        <Button>
+          <Link href="/notes">Notes</Link>
+        </Button>
+        <UserButton />
+      </SignedIn>
+    </nav>
+  );
+}
